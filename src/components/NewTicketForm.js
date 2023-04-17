728c5700cba2476224974e80488e5e7bb47ff31f
OLGON92 <ogon318@gmail.com>
2023-04-17 16:40:46 -0700

    remove uuid

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { v4 } from "uuid";
 import ReusableForm from "./ReusableForm";
 
 function NewTicketForm(props) {
@@ -11,7 +10,6 @@ function NewTicketForm(props) {
       names: event.target.names.value,
       location: event.target.location.value,
       issue: event.target.issue.value,
-      id: v4()
     });
   }
   return (
